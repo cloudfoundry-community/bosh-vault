4f2405fffe8d5fe47ffe118e2b1ee6a81b2e1191
Matt Surabian <msurabian@zipcar.com>
2018-10-04 13:26:01 -0400

    Expose generic request parse errors

--- types/generic.go
+++ types/generic.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
@@ -15,7 +16,7 @@ type GenericCredentialRequest interface {
 	Generate() error
 }
 
-func ParseGenericCredentialRequest(requestBody string) GenericCredentialRequest {
+func ParseGenericCredentialRequest(requestBody string) (GenericCredentialRequest, error) {
 	var g GenericCredentialPostRequest
 	err := json.Unmarshal([]byte(requestBody), &g)
 	if err != nil {
@@ -24,21 +25,21 @@ func ParseGenericCredentialRequest(requestBody string) GenericCredentialRequest 
 	switch g.Type {
 	case CertificateType:
 		var certificate CertificateRequest
-		json.Unmarshal([]byte(requestBody), &certificate)
-		return &certificate
+		err = json.Unmarshal([]byte(requestBody), &certificate)
+		return &certificate, err
 	case PasswordType:
 		var password PasswordRequest
-		json.Unmarshal([]byte(requestBody), &password)
-		return &password
+		err = json.Unmarshal([]byte(requestBody), &password)
+		return &password, err
 	case SshKeypairType:
 		var ssh SshKeypairRequest
-		json.Unmarshal([]byte(requestBody), &ssh)
-		return &ssh
+		err = json.Unmarshal([]byte(requestBody), &ssh)
+		return &ssh, err
 	case RsaKeypairType:
 		var rsa RsaKeypairRequest
-		json.Unmarshal([]byte(requestBody), &rsa)
-		return &rsa
+		err = json.Unmarshal([]byte(requestBody), &rsa)
+		return &rsa, err
 	default:
-		return nil
+		return nil, errors.New(fmt.Sprintf("Request type not supported! Must be one of: %s, %s, %s, %s", CertificateType, PasswordType, SshKeypairType, RsaKeypairType))
 	}
 }
--- types/generic_test.go
+++ types/generic_test.go
@@ -12,34 +12,39 @@ var _ = Describe("Generic", func() {
 	Describe("ParseGenericCredentialRequest", func() {
 		Context("valid certificate requests", func() {
 			It("parses a certificate request object", func() {
-				credential := vcfcsTypes.ParseGenericCredentialRequest(fakes.RegularCertRequestBody)
+				credential, err := vcfcsTypes.ParseGenericCredentialRequest(fakes.RegularCertRequestBody)
+				Expect(err).ToNot(HaveOccurred())
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.CertificateRequest)(nil))))
 
-				credential = vcfcsTypes.ParseGenericCredentialRequest(fakes.IntermediateCaRequestBody)
+				credential, err = vcfcsTypes.ParseGenericCredentialRequest(fakes.IntermediateCaRequestBody)
+				Expect(err).ToNot(HaveOccurred())
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.CertificateRequest)(nil))))
 
-				credential = vcfcsTypes.ParseGenericCredentialRequest(fakes.RootCaRequestBody)
+				credential, err = vcfcsTypes.ParseGenericCredentialRequest(fakes.RootCaRequestBody)
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.CertificateRequest)(nil))))
 			})
 		})
 
 		Context("valid password requests", func() {
 			It("parses a password request object", func() {
-				credential := vcfcsTypes.ParseGenericCredentialRequest(fakes.PasswordPostRequestBody)
+				credential, err := vcfcsTypes.ParseGenericCredentialRequest(fakes.PasswordPostRequestBody)
+				Expect(err).ToNot(HaveOccurred())
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.PasswordRequest)(nil))))
 			})
 		})
 
 		Context("valid ssh key requests", func() {
 			It("parses a ssh keypair object", func() {
-				credential := vcfcsTypes.ParseGenericCredentialRequest(fakes.SshKeypairRequestBody)
+				credential, err := vcfcsTypes.ParseGenericCredentialRequest(fakes.SshKeypairRequestBody)
+				Expect(err).ToNot(HaveOccurred())
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.SshKeypairRequest)(nil))))
 			})
 		})
 
 		Context("valid rsa key requests", func() {
 			It("parses a rsa keypair object", func() {
-				credential := vcfcsTypes.ParseGenericCredentialRequest(fakes.RsaKeyPairRequestBody)
+				credential, err := vcfcsTypes.ParseGenericCredentialRequest(fakes.RsaKeyPairRequestBody)
+				Expect(err).ToNot(HaveOccurred())
 				Expect(reflect.TypeOf(credential)).To(Equal(reflect.TypeOf((*vcfcsTypes.RsaKeypairRequest)(nil))))
 			})
 		})
