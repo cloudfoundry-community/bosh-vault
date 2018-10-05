3d899c58c805af361c079060e02ec34154b99bdb
Matt Surabian <msurabian@zipcar.com>
2018-10-05 14:02:55 -0400

    Add config package

--- config/config.go
+++ config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"github.com/micro/go-config"
+	"github.com/micro/go-config/source/file"
+)
+
+const default_api_listen_address = "localhost:1337"
+const default_log_level = "DEBUG"
+
+type Configuration struct {
+	ApiListenAddress string `json:"listen_addr" yaml:"listen_addr"`
+	LogLevel         string `json:"log_level" yaml:"log_level"`
+}
+
+func GetConfig(configFilePath *string) Configuration {
+	var vcfcsConfig Configuration
+	vcfcsConfig.ApiListenAddress = default_api_listen_address
+	vcfcsConfig.LogLevel = default_log_level
+
+	if *configFilePath == "" {
+		return vcfcsConfig
+	} else {
+		conf := config.NewConfig()
+		conf.Load(file.NewSource(
+			file.WithPath(*configFilePath),
+		))
+		conf.Scan(&vcfcsConfig)
+		return vcfcsConfig
+	}
+}
