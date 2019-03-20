7b164d04fb82c6875d577e441b7a869912415e62
Matt Surabian <matt@mattsurabian.com>
2019-03-19 21:53:15 -0400

    Add ssh key tests

--- types/ssh_keypair_test.go
+++ types/ssh_keypair_test.go
@@ -1,11 +1,15 @@
 package types_test
 
 import (
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/zipcar/bosh-vault/store"
 	"github.com/zipcar/bosh-vault/types"
 	fakes "github.com/zipcar/bosh-vault/types/typesfakes"
+	"golang.org/x/crypto/ssh"
 )
 
 var _ = Describe("SSH", func() {
@@ -22,6 +26,21 @@ var _ = Describe("SSH", func() {
 
 			It("returns true when Validate is called", func() {
 				Expect(SshRequest.Validate()).To(BeTrue())
+			})
+
+			It("generates a real ssh key", func() {
+				cred, err := SshRequest.Generate(&store.SimpleStore{})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cred.(types.SshKeypairRecord).PublicKey).ToNot(BeEmpty())
+				Expect(cred.(types.SshKeypairRecord).PrivateKey).ToNot(BeEmpty())
+				Expect(cred.(types.SshKeypairRecord).PublicKeyFingerprint).ToNot(BeEmpty())
+				privKeyPem, _ := pem.Decode([]byte(cred.(types.SshKeypairRecord).PrivateKey))
+				privKey, err := x509.ParsePKCS1PrivateKey(privKeyPem.Bytes)
+				Expect(err).ToNot(HaveOccurred())
+				pubKey, err := ssh.NewPublicKey(&privKey.PublicKey)
+				pubKeyBytes := ssh.MarshalAuthorizedKey(pubKey)
+				Expect(string(pubKeyBytes)).To(Equal(cred.(types.SshKeypairRecord).PublicKey))
+				Expect(ssh.FingerprintLegacyMD5(pubKey)).To(Equal(cred.(types.SshKeypairRecord).PublicKeyFingerprint))
 			})
 		})
 	})
