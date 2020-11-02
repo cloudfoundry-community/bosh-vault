c0080fc75d862bb699175238f90b132d767db4ba
Matt Surabian <msurabian@zipcar.com>
2020-11-02 11:07:44 -0500

    Restrict cipher suite list

--- server/server.go
+++ server/server.go
@@ -102,6 +102,12 @@ func ListenAndServe(bvConfig config.Configuration) {
 				MaxVersion:               tls.VersionTLS12,
 				PreferServerCipherSuites: true,
 				Certificates:             []tls.Certificate{cert},
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				},
 			}
 
 			server := &http.Server{
